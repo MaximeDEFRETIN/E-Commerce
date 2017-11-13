b6922787eb6c9ee272b844aec2fbf788b9b1da33
AndreVictor60 <andre.victor60@free.fr>
2017-11-13 11:09:27 +0100

    version 0.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(function(){
-  var click = 0;
+  var valeurtotal = 0;
   // Quand on clique sur le panier d'un article
    $("figure > button").on("click",function(id){
      // Recuperer id du parent de input
-     var id=$(this).parent("figure").attr("id");;;
+     var id=$(this).parent("figure").attr("id");
      // recuperer src de article actuelle
       var img =$('#'+id+' > img').attr('src');
       // recuperer le price
@@ -14,50 +14,76 @@ $(function(){
       price = price[2];
       var ref =$('#'+id+'> img').attr('ref');
       var pricetotal = price;
+      $('#blabla').find('#test').text(valeurtotal);
       if($('#tr'+id+'').length > 0 ){
-        alert('L\'article a déjà étè selectionner.')
+        alert('L\'article est déjà dans le panier.');
         var value = $('#tabl'+id+' > input').val();
         value++;
         $('#tabl'+id+' > input').val(value);
+        var priceactuel = price * value;
+        valeurtotal = valeurtotal + parseInt(priceactuel);
+        $('#blabla').find('#test').text(valeurtotal);
       }else {
-      $("tbody").append( "<tr id='tr"+id+"'>" +"<td><img src=\""+ img +"\" class=\"imgbasket\"/></td>"+"<td>"+ref+"</td>"+"<td id='price"+id+"'>"+price+"</td>" +"<td id='tabl"+id+"'><button class='plus'>+</button><input class='price' value='1' disabled/><button class='supp'>-</button></td>"+"<td id='priceTotal"+id+"'><p>"+pricetotal+"</p></td>"+"<td id='supp"+id+"'><a><i class=\"icofont icofont-delete-alt\"></i></a></td>"+"</tr>" );
-      alert('L\'article a étè ajouter au panier.');
+        $("tbody").append( "<tr id='tr"+id+"'>" +"<td><img src=\""+ img +"\" class=\"imgbasket\"/></td>"+"<td>"+ref+"</td>"+"<td id='price"+id+"'>"+price+"</td>" +"<td id='tabl"+id+"'><button class='plus'>+</button><input class='price' value='1' disabled/><button class='supp'>-</button></td>"+"<td id='priceTotal"+id+"'><p>"+pricetotal+"</p></td>"+"<td id='supp"+id+"'><a><i class=\"icofont icofont-delete-alt\"></i></a></td>"+"</tr>" );
+        alert('L\'article a étè ajouter au panier.');
+        valeurtotal = $('#blabla').find('#test').text();
+        console.log('La valeur 1 temps est '+ valeurtotal);
+        valeurtotal = parseFloat(valeurtotal) + parseInt(price);
+        console.log('Le prix est '+valeurtotal);
+        $('#blabla').find('#test').text(valeurtotal);
+
       };
 
-      // Augmenter la quantité d'un article
-      $('#tabl'+id+' > .plus').click(function() {
-        // Récuperer la value de l'input de l'article
-        var value = $('#tabl'+id+' > input').val();
-        // Prend la value et on fait +1
-        value = parseInt(value) + 1;
-        console.log(value);
-        // Ajouter la value dans l'input
-        $('#tabl'+id+' > input').val(value);
-        var valeur = $('#price'+id+'').text();
-        valeur = parseInt(valeur);
-        pricetotal = valeur * value;
-        $('#priceTotal'+id+'> p').text(pricetotal);
-    });
-    // Baisse la quantité d'une article
-    $('#tabl'+id+' > .supp').click(function() {
-      // Récuperer la value de l'input de l'article
-      var value = $('#tabl'+id+' > input').val();
-      // Prend la value et on fait -1
-      value--;
-      // Ajouter la value dans l'input
-      $('#tabl'+id+' > input').val(value);
-      pricetotal = pricetotal - price;
-      $('#priceTotal'+id+'> p').text(pricetotal);
-      // Si value de l'input = 0 alors on supprime la ligne du tableau
-      if(value == 0){
-        $('#tr'+id+'').remove();
-      }
-    });
+
     $('#supp'+id+' > a').click(function(){
       $('#tr'+id+'').remove();
     });
   });
+$('#myBtn').click(function () {
+  // Augmenter la quantité d'un article
+  $('.plus').click(function() {
+    // Récuperer la value de l'input de l'article
+    var valeurtotal = $('#blabla').find('#test').text();
+    var id =$(this).parent('td').attr('id');
+    var value = $('#'+id+' > input').val();
+    // Prend la value et on fait +1
+    value++;
+    // Ajouter la value dans l'input
+    $('#'+id+' > input').val(value);
+    var length = id.length;
+    id = id.substring(4,length);
+    var valeur = $('#price'+id+'').text();
+    valeur = parseInt(valeur);
+    pricetotal = valeur * value;
+    $('#priceTotal'+id+'> p').text(pricetotal);
+    valeurtotal = $('#blabla').find('#test').text();
+    valeurtotal = parseFloat(valeurtotal) + valeur;
+    $('#blabla').find('#test').text(valeurtotal);
 
+  });
+  // Baisse la quantité d'une article
+  $('.supp').click(function() {
+    // Récuperer la value de l'input de l'article
+    var id =$(this).parent('td').attr('id');
+    var value = $('#'+id+' > input').val();
+    // Prend la value et on fait -1
+    value--;
+    // Ajouter la value dans l'input
+    $('#'+id+' > input').val(value);
+    var length = id.length;
+    id = id.substring(4,length);
+    var price = $('#price'+id+'').text();
+    pricetotal = pricetotal - price;
+    $('#priceTotal'+id+'> p').text(pricetotal);
+    valeurtotal = $('#blabla').find('#test').text();
+    valeurtotal = parseFloat(valeurtotal) - price ;
+    $('#blabla').find('#test').text(valeurtotal);
+    // Si value de l'input = 0 alors on supprime la ligne du tableau
+    if(value == 0){
+      $('#tr'+id+'').remove();
+    }
+  });
+});
   $("#myBtn").click(function(){
     $("#myModal").modal();
   });
@@ -67,20 +93,4 @@ $(function(){
    }, function() {
      $('#'+this.id +' span').hide();
  });
-
- /*$( "#dialog" ).dialog({
-   autoOpen: false,
-   show: {
-     effect: "blind",
-     duration: 1000
-   },
-   hide: {
-     effect: "explode",
-     duration: 1000
-   }
- });
-
- $( "#opener" ).on( "click", function() {
-   $( "#dialog" ).dialog( "open" );
- });*/
 });
